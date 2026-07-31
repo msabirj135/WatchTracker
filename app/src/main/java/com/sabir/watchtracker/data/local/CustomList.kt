6b1f14f823cadf8c80da84afa90b0ade43be8da6
Muhamed Sabir <wanderlust.msj@gmail.com>
2026-07-31 23:49:18 +0530

    Add CustomList.kt

---
+++ app/src/main/java/com/sabir/watchtracker/data/local/CustomList.kt
@@ -0,0 +1,14 @@
+package com.sabir.watchtracker.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "custom_lists")
+data class CustomList(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val description: String,
+    val createdAt: Long,
+    val updatedAt: Long
+)
